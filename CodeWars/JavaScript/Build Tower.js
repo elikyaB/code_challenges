931b53fac3bedd05cb929519de1000c80de4ba59
elikya <elikyabokanga@gmail.com>
2021-12-30 23:30:59 -0500

    build tower js complete

---
+++ CodeWars/JavaScript/Build Tower.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/576757b1df89ecf5bd00073b/train/javascript
+function towerBuilder(nFloors) {
+    const floorLength = 1+2*(nFloors-1)
+    const tower = []
+    let i = 1
+    while (tower.length < nFloors) {
+        let space = floorLength - i
+        let floor = ""
+        if (space>0) {floor = floor.concat(' '.repeat(space/2))}
+        floor = floor.concat('*'.repeat(i))
+        if (space>0) {floor = floor.concat(' '.repeat(space/2))}
+        tower.push(floor)
+        i+=2
+        
+    }
+    return tower
+}
+
+arr = towerBuilder(2)
+console.log(arr)
---
+++ CodeWars/Python/Build Tower.py
@@ -0,0 +1,7 @@
+def tower_builder(n_floors):
+    tower = []
+    floor_length = 1 + 2*(n_floors-1)
+    stars = ''
+    i = 1
+    while i <= floor_length:
+        
