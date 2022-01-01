82e1fb272daf22cd73c282abac2f0b45f4f3dd40
elikya <elikyabokanga@gmail.com>
2022-01-01 18:34:12 -0500

    Rot13 javascript

---
+++ CodeWars/JavaScript/Rot13.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/530e15517bc88ac656000716/train/javascript
+function rot13(message){
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+    return message.split('').map((m) => {
+        let capital = false
+        if (alphabet.indexOf(m) === -1) {
+            if (alphabet.includes(m.toLowerCase())) {capital = !capital}
+            else {return m}
+        }
+        const l = capital? m.toLowerCase() : m
+        const cipher = alphabet.indexOf(l)+13 > 25? alphabet[alphabet.indexOf(l)-13] : alphabet[alphabet.indexOf(l)+13]
+        return capital? cipher.toUpperCase() : cipher
+    }).join('')
+}
+
+console.log(rot13("Grfg"))
