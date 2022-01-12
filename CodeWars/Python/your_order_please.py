bf3c2d236843adadf3dc37a2ceae59d938d03fec
elikya <elikyabokanga@gmail.com>
2022-01-12 10:44:57 -0500

    renamed via Python convention, challenge of the day done for python

---
+++ CodeWars/Python/your_order_please.py
@@ -0,0 +1,11 @@
+## https://www.codewars.com/kata/55c45be3b2079eccff00010f/train/python
+import re
+def order(sentence):
+    if sentence == '':
+        return ''
+    arr = sentence.split()
+    arr.sort(key=lambda x : int(re.findall('\d',x)[0]))
+    return ' '.join(arr)
+    
+print(order('is2 Thi1s T4est 3a'))
+## need to figure out why method chaining here doesn't work
