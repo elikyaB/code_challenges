6e7df1e662c8da1ab96e0356abe617b45df87e69
elikya <elikyabokanga@gmail.com>
2022-03-31 21:00:09 -0400

    proton.ai interview questions

---
+++ CodeWars/Python/range_extraction.py
@@ -0,0 +1,14 @@
+## https://www.codewars.com/kata/51ba717bb08c1cd60f00002f/train/python
+def solution(args):
+    result = ''
+    start = None
+    prev = None
+        ## if previous == None, start = n
+        ## else if start == prev+1, prev = n
+        ## else start = n
+        ##
+    return result
+
+print(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]))
+print(solution([ -71, -70, -68, -67, -64, -62, -59, -57, -56, -55, -53 ]))
+print(solution([-80,-78,-75,-74,-72,-69,-66,-64,-63,-62,-59,-56,-55,-52,-51,-49,-48,-46,-44,-43]))
