fdca9460cd39c05698f6dcd3290c04e14819ac12
elikya <elikyabokanga@gmail.com>
2022-03-31 21:06:58 -0400

    linter fix

--- Interview/proton.ai/server.js
+++ Interview/proton.ai/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 6969
+// d
 
 const plants = ['Monstera Deliciosa', 'Corpse Flower', 'Elephant-Foot Yam', "Witches' Butter"]
 
