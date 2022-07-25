f73a0058387dbe467ba6d79d223e7abcdff5602d
elikya <elikyabokanga@gmail.com>
2022-07-25 19:33:16 -0400

    proper mergeSort

--- Interview/Google/mergeSort.js
+++ Interview/Google/mergeSort.js
@@ -1,37 +1,35 @@
 function mergeSort (array) {
 
-    let sorted = []
-
     const separate = (arr) => {
         let len = arr.length
-        if (len<=1) {return sorted.push(arr)}
+        if (len<=1) {return arr}
         let half = Math.ceil((len-1)/2)
         let left = arr.slice(0,half)
         let right = arr.slice(half)
-        console.log(half, left, right)
-        separate(left), separate(right)
+        return merge(separate(left), separate(right))
     }
 
-    const merge = (arr) => {
-        let len = arr.length
-        let results = []
-        let swapHappened = false
-        while (results.length < len) {
-            console.log(arr[0], arr[1])
-            if (arr[0] <= arr[1] || arr[1] == undefined) {results.push(...arr.splice(0,1))}
-            else {results.push(...arr.splice(1,1)); swapHappened = true;}
-            console.log(arr, results, swapHappened)
+    const merge = (left, right) => {
+        const merged = []
+        let l = 0, r = 0
+        while (merged.length < left.length+right.length) {
+            if (left[l] <= right[r]) {
+                if (left[l]==undefined) {merged.push(right[r]); r+=1}
+                else {merged.push(left[l]); l+=1}
+            }
+            else {
+                if (right[r]==undefined) {merged.push(left[l]); l+=1}
+                else {merged.push(right[r]); r+=1}
+            }
         }
-        if (swapHappened) {return merge(results)}
-        else {return results}
+        return merged
     }
 
-    return merge(array)
+    return separate(array)
 }
 
 // TESTS
 const generateArray = require('./generateArray')
 const randomArray = generateArray(10, -10, 10)
-// console.log(randomArray)
-// console.log(mergeSort(randomArray))
-console.log(mergeSort([10,9,8,7,6,5,4,3,2,1,0]))
+console.log(randomArray)
+console.log(mergeSort(randomArray))
