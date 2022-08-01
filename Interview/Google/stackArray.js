a1b430955c27d8a52c6fa4cdc7d7dfaf527d04b8
elikya <elikyabokanga@gmail.com>
2022-07-31 23:22:00 -0400

    queue and stack DS implementations

---
+++ Interview/Google/stackArray.js
@@ -0,0 +1,21 @@
+class Stack {
+    constructor() {
+      this.items = []
+    }
+  
+    push(item){
+      this.items.push(item)
+    }
+  
+    pop(){
+      return this.items.pop()
+    }
+  
+    peek(){
+      return this.items[this.items.length - 1]
+    }
+  
+    isEmpty(){
+      return this.items.length === 0
+    }
+}
